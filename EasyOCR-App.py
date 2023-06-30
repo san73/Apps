0fd03a8840a1529c446b124cafa99158dc369609
san73 <punchsan73@gmail.com>
2023-06-30 14:07:53 +0800

    Update EasyOCR-App.py

--- EasyOCR-App.py
+++ EasyOCR-App.py
@@ -48,16 +48,15 @@ if upl_image is not None:
         input_image = Image.open(upl_image) #read image
         st.image(input_image) #display image
     
-        with st.spinner("AI is at Work! "): 
-    
-            result = reader.readtext(np.array(input_image))
-    
-            result_text = [] #empty list for results
-    
-            for text in result:
-                result_text.append(text[1])
-            st.write("Result:")
-            st.write(result_text)
+        result = reader.readtext(np.array(input_image))
+
+        result_text = [] #empty list for results
+
+        for text in result:
+            result_text.append(text[1])
+        st.write("Result:")
+        st.write(result_text)
+        
     except:
         pass
 else:
